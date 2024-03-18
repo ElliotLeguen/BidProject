1ee304c42ebdac912807226f2cf0e95ee1f75ae0
eleguen2023 <elliot.leguen2023@campus-eni.fr>
2024-03-18 16:50:34 +0100

    first Elliot

---
+++ src/main/java/projet/bid_pro/bll/contexte/UtilisateurServiceImpl.java
@@ -0,0 +1,21 @@
+package projet.bid_pro.bll.contexte;
+
+import org.springframework.stereotype.Service;
+
+import projet.bid_pro.bo.Utilisateur;
+import projet.bid_pro.dal.UtilisateurDAO;
+
+@Service
+public class UtilisateurServiceImpl implements UtilisateurService {
+
+	private UtilisateurDAO utilisateurDAO;
+
+	public UtilisateurServiceImpl(UtilisateurDAO utilisateurDAO) {
+		this.utilisateurDAO = utilisateurDAO;
+	}
+
+	@Override
+	public Utilisateur charger(String email) {
+		return utilisateurDAO.read(email);
+	}
+}
---
+++ src/main/java/projet/bid_pro/bo/ArticleVendu.java
@@ -0,0 +1,118 @@
+package projet.bid_pro.bo;
+
+import java.util.Date;
+
+public class ArticleVendu {
+    private int noArticle;
+    private String nomArticle;
+    private String description;
+    private Date dateDebutEncheres;
+    private Date dateFinEncheres;
+    private Integer prixInitial;
+    private Integer prixVente;
+    private int noUtilisateur;
+    private int noCategorie;
+
+    // Constructeur
+    public ArticleVendu(int noArticle, String nomArticle, String description, Date dateDebutEncheres,
+                        Date dateFinEncheres, Integer prixInitial, Integer prixVente,
+                        int noUtilisateur, int noCategorie) {
+        this.noArticle = noArticle;
+        this.nomArticle = nomArticle;
+        this.description = description;
+        this.dateDebutEncheres = dateDebutEncheres;
+        this.dateFinEncheres = dateFinEncheres;
+        this.prixInitial = prixInitial;
+        this.prixVente = prixVente;
+        this.noUtilisateur = noUtilisateur;
+        this.noCategorie = noCategorie;
+    }
+
+    // Getters et Setters
+    public int getNoArticle() {
+        return noArticle;
+    }
+
+    public void setNoArticle(int noArticle) {
+        this.noArticle = noArticle;
+    }
+
+    public String getNomArticle() {
+        return nomArticle;
+    }
+
+    public void setNomArticle(String nomArticle) {
+        this.nomArticle = nomArticle;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDateDebutEncheres() {
+        return dateDebutEncheres;
+    }
+
+    public void setDateDebutEncheres(Date dateDebutEncheres) {
+        this.dateDebutEncheres = dateDebutEncheres;
+    }
+
+    public Date getDateFinEncheres() {
+        return dateFinEncheres;
+    }
+
+    public void setDateFinEncheres(Date dateFinEncheres) {
+        this.dateFinEncheres = dateFinEncheres;
+    }
+
+    public Integer getPrixInitial() {
+        return prixInitial;
+    }
+
+    public void setPrixInitial(Integer prixInitial) {
+        this.prixInitial = prixInitial;
+    }
+
+    public Integer getPrixVente() {
+        return prixVente;
+    }
+
+    public void setPrixVente(Integer prixVente) {
+        this.prixVente = prixVente;
+    }
+
+    public int getNoUtilisateur() {
+        return noUtilisateur;
+    }
+
+    public void setNoUtilisateur(int noUtilisateur) {
+        this.noUtilisateur = noUtilisateur;
+    }
+
+    public int getNoCategorie() {
+        return noCategorie;
+    }
+
+    public void setNoCategorie(int noCategorie) {
+        this.noCategorie = noCategorie;
+    }
+
+    @Override
+    public String toString() {
+        return "ArticleVendu{" +
+                "noArticle=" + noArticle +
+                ", nomArticle='" + nomArticle + '\'' +
+                ", description='" + description + '\'' +
+                ", dateDebutEncheres=" + dateDebutEncheres +
+                ", dateFinEncheres=" + dateFinEncheres +
+                ", prixInitial=" + prixInitial +
+                ", prixVente=" + prixVente +
+                ", noUtilisateur=" + noUtilisateur +
+                ", noCategorie=" + noCategorie +
+                '}';
+    }
+}
