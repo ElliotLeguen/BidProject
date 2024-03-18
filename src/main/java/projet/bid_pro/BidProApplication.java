4e17a09afd0145779a2c09703dbb26f379415d42
eleguen2023 <elliot.leguen2023@campus-eni.fr>
2024-03-18 13:59:50 +0100

    first Elliot

--- src/main/java/projet/bid_pro/BidProApplication.java
+++ src/main/java/projet/bid_pro/BidProApplication.java
@@ -9,6 +9,7 @@ public class BidProApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BidProApplication.class, args);
 		System.out.println("Hello World");
+		System.out.println("Hola todos");
 	}
 
 }
