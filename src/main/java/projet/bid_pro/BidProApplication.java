06778d44da7ec5b8528a053369ddf5920cd8aedb
Esteban RAUTUREAU <esteban.rautureau2023@campus-eni.fr>
2024-03-18 11:30:22 +0100

    first commit

---
+++ src/main/java/projet/bid_pro/BidProApplication.java
@@ -0,0 +1,13 @@
+package projet.bid_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidProApplication.class, args);
+	}
+
+}
