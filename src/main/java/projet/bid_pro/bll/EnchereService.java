f165a0cf4facb61676aa74645491aebb8b4acf94
eleguen2023 <elliot.leguen2023@campus-eni.fr>
2024-03-19 09:29:52 +0100

    base projet

---
+++ src/main/java/projet/bid_pro/bll/EnchereService.java
@@ -0,0 +1,27 @@
+//package projet.bid_pro.bll;
+//
+//import java.util.List;
+//
+//
+//
+//public interface EnchereService {
+//	List<Film> consulterFilms();
+//
+//	Film consulterFilmParId(long id);
+//
+//	List<Genre> consulterGenres();
+//
+//	List<Participant> consulterParticipants();
+//
+//	Genre consulterGenreParId(long id);
+//
+//	Participant consulterParticipantParId(long id);
+//
+//	void creerFilm(Film film);
+//
+//	String consulterTitreFilm(long id);
+//
+//	void publierAvis(Avis avis, long idFilm);
+//
+//	List<Avis> consulterAvis(long idFilm);
+//}
